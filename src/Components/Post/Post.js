ec5198d5f6ef1ce5f6a23c22e81a6a89f2c4e346
ma-foyez <mafoyez-bd@gmail.com>
2020-09-05 02:27:26 +0600

    Social Body Assignment

---
+++ src/Components/Post/Post.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+        // maxWidth: 327,
+    },
+    media: {
+        height: 140,
+    },
+});
+
+const Post = (props) => {
+    const classes = useStyles();
+    const { userId, title, body } = props.post;
+
+    // console.log(props.post)
+
+    return (
+        <Grid item xs={12} sm={6} md={3}>
+            <Card className={classes.root} style={{boxShadow: "4px 6px 12px -4px rgba(0,0,0,0.75)", width: "90%",}}>
+                <CardActionArea>
+                    <CardContent style={{height: 220}}>
+                        <Typography gutterBottom variant="h5" component="h3" style={{ textTransform: "capitalize", textAlign: "center", color: "#ff4a03" }}> {title} </Typography> <br/>
+                        <Typography variant="body2" color="textSecondary" component="p"> {body} </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button variant="contained" color="secondary"> Details </Button>
+                </CardActions>
+            </Card>
+        </Grid>
+    );
+};
+
+export default Post;
---
+++ src/Components/SocialContainer/SocialContainer.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import Post from '../Post/Post';
+import { Grid, Paper } from '@material-ui/core';
+
+const SocialContainer = () => {
+    const [posts, setPosts] = useState([]);
+    // const [simplePost, setSimplePost] = useState([]);
+    // const [finalPost, setFinalPost] = useState([]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(data => setPosts(data))
+            .catch(error => console.log(error))
+    }, [])
+
+    return (
+        <div>
+            <Grid container spacing={2} style={{ marginTop: "10px" }}>
+                {
+                    posts.map(post => <Post post={post}></Post>)
+                }
+            </Grid>
+
+        </div>
+    );
+};
+
+export default SocialContainer;
